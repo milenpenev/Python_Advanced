232259e8e0de8e17d8da5297bacc757ddc29225d
Milen Penev <milennpenev@gmail.com>
2021-06-02 17:31:57 +0300

    Update 04. Symbol in Matrix.py

---
+++ Multidimensional Lists/04. Symbol in Matrix.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+matrix = []
+
+for row in range(n):
+    matrix.append(list(input()))
+
+symbol = input()
+position = None
+
+
+for row in range(n):
+    for col in range(n):
+        if matrix[row][col] == symbol:
+            position = (row, col)
+            break
+    if position:
+        break
+
+# (position if position else f"{symbol} does not occur in the matrix") same as below
+if position:
+    print(position)
+else:
+    print(f"{symbol} does not occur in the matrix")
