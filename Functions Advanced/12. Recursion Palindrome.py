b9a03a70d33355adf6abd06bb2898bcf88816bb3
Milen Penev <milennpenev@gmail.com>
2021-06-09 13:09:16 +0300

    update

--- Functions Advanced/12. Recursion Palindrome.py
+++ Functions Advanced/12. Recursion Palindrome.py
@@ -1,1 +1,15 @@
-def
+def palindrome(word, index):
+    left = index
+    right = len(word) - 1 - index
+
+    if left >= right:
+        return f"{word} is a palindrome"
+
+    right_letter = word[len(word)-1-index]
+    left_letter = word[index]
+    if left_letter != right_letter:
+        return f"{word} is not a palindrome"
+    return palindrome(word, index + 1)
+
+print(palindrome("abcba", 0))
+print((palindrome("peter", 0)))
