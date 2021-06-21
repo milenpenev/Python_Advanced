4073d10c54f48d7ce85282ccaa3d499c151e1f0f
Milen Penev <milennpenev@gmail.com>
2021-06-21 22:09:19 +0300

    complete tic-tac-toe, without error if place is unavailable

--- Workshop/core/helpers.py
+++ Workshop/core/helpers.py
@@ -1,6 +1,13 @@
 from Workshop.core.constans import position_mapper
 
 
+def is_board_full(board):
+    is_full = True
+    for row in board:
+        if " " in row:
+            is_full = False
+    return is_full
+
 def print_current_board_state(board):
     [print(f"| {' | '.join(el)} |") for el in board]
 
@@ -8,3 +15,59 @@ def print_current_board_state(board):
 def get_row_col(position):
     row, col = position_mapper[position]
     return row, col
+
+
+def is_row_winner(board):
+    is_found = False
+    for row in board:
+        if row.count("X") == len(row) or row.count("O") == len(row):
+            is_found = True
+
+    return is_found
+
+def is_col_winner(board):
+    is_found = False
+    first_col = [board[0][0], board[1][0], board[2][0]]
+    second_col = [board[0][1], board[1][1], board[2][1]]
+    third_col = [board[0][2], board[1][2], board[2][2]]
+
+    if first_col.count("X") == len(board) or first_col.count('O') == len(board):
+        is_found = True
+    elif second_col.count("X") == len(board) or second_col.count('O') == len(board):
+        is_found = True
+    elif third_col.count("X") == len(board) or third_col.count('O') == len(board):
+        is_found = True
+    return is_found
+
+
+def is_primary_diagonal_winner(board):
+    current_values = []
+    for row in range(len(board)):
+        current_values.append(board[row][row])
+    if current_values.count('X') == len(board) or current_values.count('O') == len(board):
+        return True
+    return False
+
+
+def is_secondary_diagonal_winner(board):
+    current_values = []
+    n = len(board)
+    for index in range(n):
+        current_values.append(board[n-index-1][index])
+    if current_values.count('X') == len(board) or current_values.count('O') == len(board):
+        return True
+    return False
+
+
+def is_winner(board):
+    if is_row_winner(board):
+        return True
+    elif is_col_winner(board):
+        return True
+    elif is_primary_diagonal_winner(board):
+        return True
+    elif is_secondary_diagonal_winner(board):
+        return True
+    elif is_board_full(board):
+        print("No winner!")
+    return False
--- Workshop/core/logic.py
+++ Workshop/core/logic.py
@@ -1,5 +1,5 @@
 from Workshop.core.validators import is_position_in_range, is_place_available
-from Workshop.core.helpers import get_row_col, print_current_board_state
+from Workshop.core.helpers import get_row_col, print_current_board_state, is_winner
 
 
 def play(players, board, turns):
@@ -13,7 +13,9 @@ def play(players, board, turns):
                 row, col = get_row_col(position)
                 board[row][col] = players[current_player_name]
                 print_current_board_state(board)
-
+                if is_winner(board):
+                    print(f"{current_player_name} wins!")
+                    exit(0)
             # check if there is a winner
             # check if there is no winner if the board is full
         else:
